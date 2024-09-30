3dcf4951b0779d78ac8f85f0d7ebd9fd9caa5a12
Cesar-eav <cesar.eav@hotmail.es>
2024-09-30 01:17:09 -0300

    Update

--- FuncionesBD.py
+++ FuncionesBD.py
@@ -62,17 +62,30 @@ def add_producto():
             except ValueError:
                 print("ValueError: Debes ingresar un número válido (1 o 2).")
 
+
+#FUNCION CREA CATEGORIAS INGRESADAS EN MAIN,
+#EL USUARIO NO PUEDE CREAR NUEVAS CATEGORIAS.
 def add_categoria(nombre):
     try:
         conn= sqlite3.connect('productos.db')
         cursor = conn.cursor()
-        cursor.execute('''
-                    
-        INSERT INTO categorias (nombre) VALUES (?)
-                ''', (nombre,))
+        
+        cursor.execute('SELECT id FROM categorias WHERE nombre = ?', (nombre,))
+        categoria_existente = cursor.fetchone()
+        
+        #EVITAR CREAR CATEGORIAS EXISTENTES AL INICIAR PROGRAMA
+        if categoria_existente:
+            print(f"La categoría '{nombre}' ya existe.")
 
-        conn.commit()
-        print(f"Categoria {nombre}  agregado exitosamente")
+        else:
+            cursor.execute('''
+                    
+            INSERT INTO categorias (nombre) VALUES (?)
+                    ''', (nombre,))
+
+            conn.commit()
+            print(f"Categoria {nombre}  agregado exitosamente")
+            
     except sqlite3.DatabaseError as e:
         print(f'Error al crear la tabla categorias: {e}')
     
@@ -133,81 +146,151 @@ def listar_productos():
 # - Buscar productos por categoría: Consultar productos según la categoría especificada por el usuario.
 
 def search_product():
-    get_categorias()
-    categoria_id = input("Introduce la categoria a buscar: ")
-
-    try:
-        conn = sqlite3.connect('productos.db')
-        cursor = conn.cursor()
-                       
-                       
-        cursor.execute('''
-            SELECT productos.id, productos.nombre, productos.precio, productos.stock 
-            FROM productos
-            WHERE productos.categoria_id = ?
-
-        ''', (categoria_id,)) 
-
-        productos = cursor.fetchall()
-
-        if productos:
- 
-            for producto in productos:
-                 print( f"Nombre: {producto[1]}, Precio: {producto[2]}, Stock: {producto[3]},  ")
-       
-        else:
-            print(f"No existe la cateogria **")
     
-    except sqlite3.DatabaseError as e:
-        print(f"Error al buscar producto: {e}")
+    while True:  
+        conn = None
+        
+        try:       
+            get_categorias()  
+            categoria_id = int(input("Introduce el ID de la categoria: "))
+            
+            if categoria_id <=0:
+                print("Numero inválido")
+                continue
+                
+        except ValueError:
+            print("Debes ingresar un número")
+            continue
+             
+        except Exception as e:
+            print(f"Ha ocurrido un error inesperado: {e}")
+                    
 
-    finally:
-        conn.close()
+        try:
+            conn = sqlite3.connect('productos.db')
+            cursor = conn.cursor()
+                        
+                        
+            cursor.execute('''
+                SELECT productos.id, productos.nombre, productos.precio, productos.stock 
+                FROM productos
+                WHERE productos.categoria_id = ?
+
+            ''', (categoria_id,)) 
+
+            productos = cursor.fetchall()
+
+            if productos:
+    
+                for producto in productos:
+                    print( f"Nombre: {producto[1]}, Precio: {producto[2]}, Stock: {producto[3]},  ")
+        
+            else:
+                print(f"No existe la cateogria")
+        
+        except sqlite3.DatabaseError as e:
+            print(f"Error al buscar producto: {e}")
+
+        finally:
+            if conn is not None:
+                conn.close()
+            
+        while True:
+            try:
+                decision = int(input("Vuelve al menú(1) o Buscar nuevamente(2): "))
+
+                if decision == 1:
+                    mostrar_menu()
+                elif decision == 2:
+                    break
+                else:
+                    print("Opción no válida, ingresa 1 o 2.")
+            except ValueError:
+                print("ValueError: Debes ingresar un número válido (1 o 2).")
 
 
 
 # - Actualizar el precio y el stock de un producto existente.
 
 def update_prduct():
-    listar_productos()
-    id_producto = input("Dame el ID del producto a modificar: ")
-
-    conn = sqlite3.connect('productos.db')
-    cursor = conn.cursor()
-
-    cursor.execute('''
-                   
-                   SELECT * FROM productos
-                        WHERE id = ?
-                   
-                   ''', (id_producto,))
     
-    productos = cursor.fetchall()
+    while True:
+        conn = None
+        
+        try:
+            listar_productos()
+            id_producto = int(input("Dame el ID del producto a modificar: "))
+            
+            if id_producto <=0:
+                print("ID Inválido")
+                continue
+            
+        except ValueError:
+            print("Debes ingresar un numero valido")
+            continue
+        
+        except Exception as e:
+            print(f"Ha ocurrido un error inesperado: {e}")
 
-    if productos:
-        for producto in productos:
-            print("----TU PRODUCTO----")
-            print(f"Id: {producto[0]}, Nombre: {producto[1]}, Categoría: {producto[2]}, Precio: {producto[3]}, Stock: {producto[4]}")
+            
+        try:
+            print("*****************CONECTANDO***************")
+            conn = sqlite3.connect('productos.db')
+            cursor = conn.cursor()
+            cursor.execute('''
+                        
+                        SELECT * FROM productos
+                                WHERE id = ?
+                        
+                        ''', (id_producto,))
+            
+            producto = cursor.fetchall()
+            
+            
+            if producto == []:
 
-        nuevo_precio = int(input("Ingresar nuevo precio: "))
-        nuevo_stock = int(input("Ingresar Stock: "))
+                print("ID NO EXISTE.")
+                continue
+            
+            print("***************** FIN CONECTANDO***************")
 
-        cursor.execute('''
-            UPDATE productos
-                SET precio = ?, stock = ?
-                WHERE id = ?
-                       
-                
-
-        ''',(nuevo_precio,nuevo_stock,id_producto ) )
-
-        conn.commit()
-
-        print(f"Producto con ID {id_producto} ha sido actualizado con éxito.")
-        print(f"Nuevo precio: {nuevo_precio}, Nuevo stock: {nuevo_stock}")
+        except sqlite3.DatabaseError as e:
+            print(f'Error al obtener producto: {e}')
+            
+     
 
 
-        conn.close()
+
+        print(f"{producto}")
+      
+
+        if producto:
+            for p in producto:
+                print("----TU PRODUCTO----")
+                print(f"Id: {p[0]}, Nombre: {p[1]}, Categoría: {p[2]}, Precio: {p[3]}, Stock: {p[4]}")
+
+            nuevo_precio = int(input("Ingresar nuevo precio: "))
+            nuevo_stock = int(input("Ingresar Stock: "))
+
+            cursor.execute('''
+                UPDATE productos
+                    SET precio = ?, stock = ?
+                    WHERE id = ?
+                        
+                    
+
+            ''',(nuevo_precio,nuevo_stock,id_producto ) )
+
+            conn.commit()
+
+            print(f"Producto con ID {id_producto} ha sido actualizado con éxito.")
+            print(f"Nuevo precio: {nuevo_precio}, Nuevo stock: {nuevo_stock}")
+        
+        
+        else:
+            print("NO EXISTE EL PRODUCO / ID")
+
+            conn.close()
 
 
 
